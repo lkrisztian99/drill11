e9b5699b6397b879d2b58838dad84021cb18c7fb
lkrisztian99 <90862618+lkrisztian99@users.noreply.github.com>
2021-11-21 14:13:55 +0100

    Add files via upload

---
+++ Test_output.cpp
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+int main(){
+    //task 1&2
+    int birth_year = 2001;
+       cout << birth_year << "\t(decimal)\n" << 
+        hex << birth_year << "\t(hexadecimal)\n" << 
+        oct << birth_year << "\t(octal)\n";
+    
+    int my_age = 20;
+    cout << dec << my_age << endl;
+}
